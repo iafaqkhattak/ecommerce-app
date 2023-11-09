49aa633fb9701ff2dc0bb68949c194396f9a43d6
Muhammad Afaq <iafaqkhattak@gmail.com>
2023-11-09 23:25:58 +0500

    Create Controllers

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
 const app = require("./app");
+const dotenv = require("./dotenv");
+
+//Config
+
+dotenv.config({ path: "backend/config/config.env" });
+
+app.listen(process.env.port, () => {
+  console.log(`Server is running on http://localhost:${process.env.port}`);
+});
