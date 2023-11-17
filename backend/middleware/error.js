e46cfbd6ebe6bf29bb793b30f2bafb065634abf8
Muhammad Afaq <iafaqkhattak@gmail.com>
2023-11-18 01:08:53 +0500

    Handle mongoose errors

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,3 +1,4 @@
+const { JsonWebTokenError } = require("jsonwebtoken");
 const ErrorHandler = require("../utils/errorHandler");
 
 module.exports = (err, req, res, next) => {
@@ -7,6 +8,24 @@ module.exports = (err, req, res, next) => {
   // wrong MongoDb ID Error
   if (err.name === "CastError") {
     const message = `Resouce Not Found. Invalid ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  //Mongoose Duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Wrong JWT Error
+  if (err.code === "JsonWebTokenError") {
+    const message = `Json Web Token is invalid, try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  //JWT Expire Error
+  if (err.code === "TokenExpiredError") {
+    const message = `Json Web Token is expired, try again`;
     err = new ErrorHandler(message, 400);
   }
 
